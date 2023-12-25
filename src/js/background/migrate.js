2121b022228a084ebe5c864bb9a33171f49261fb
Manvel Saroyan <saroyanm@gmail.com>
2023-12-25 14:39:31 +0400

    No issue - cleanup startup migration code

--- src/js/background/migrate.js
+++ src/js/background/migrate.js
@@ -17,29 +17,6 @@
  * <http://www.gnu.org/licenses/>.
  */
 
-const {saveState} = require("../db/projects");
-const customActionsDb = require("../db/customActions");
-
-function getOldData()
-{
-  const data = JSON.parse(localStorage.getItem("data"));
-  const settings = JSON.parse(localStorage.getItem("settings"));
-  const cbaFunctions = JSON.parse(localStorage.getItem("cba-functions"));
-  return {data, settings, cbaFunctions};
-}
-
-/**
- * CBA previously were using windows.localStorage, here we ensure that user
- * won't loose their old data if migration to Chrome.storage API won't go well.
- */
-async function backup() {
-  const {data, settings, cbaFunctions} = getOldData();
-  if (data || settings || cbaFunctions) {
-    const backup = {data, settings, cbaFunctions};
-    await browser.storage.local.set({backup});
-  }
-}
-
 function migrateActions(actions) {
   if (actions && actions.length) {
     return actions.map(({data, evType, newValue}) => {
@@ -88,28 +65,4 @@ function migrateData(oldData) {
   return groups;
 }
 
-async function migrate() {
-  const {data, cbaFunctions} = getOldData();
-  if (data) {
-    await saveState(migrateData(data));
-  }
-  if (cbaFunctions) {
-    const customActions = [];
-    for (const {name, data, evType, newValue} of cbaFunctions) {
-      const [predefined] = customActionsDb.predefined
-                                          .filter(({text}) => text === name);
-      const input2 = newValue ? newValue : "";
-      const type = evType;
-      const inputs = type == "timer" ? [input2, data] : [data, input2];
-      const info = predefined ? predefined.info : {description: ""};
-      customActions.push({
-        data: {type, inputs},
-        text: name,
-        info
-      });
-    }
-    await customActionsDb.saveState(customActions);
-  }
-}
-
-module.exports = {migrate, backup, migrateData, migrateProjects};
+module.exports = {migrateData, migrateProjects};
--- tests/config.js
+++ tests/config.js
@@ -25,7 +25,6 @@ const tests = [
   {file:"options.js", name: "Testing options page generic functionality"},
   {file:"import-export.js", name: "Testing Import and Export in options page"},
   {file:"functions.js", name: "Testing functions tab in options page"},
-  {file:"migrate.js", name: "Testing data migration"},
 ];
 
 const server = "http://127.0.0.1:3001";
